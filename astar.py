cd6f3ae95721ccc55045eb38cc7cb1e3a65e2ec9
svennhv <svennhv@gmail.com>
2016-10-07 13:44:18 +0200

    deleted node.py

--- astar.py
+++ astar.py
@@ -3,26 +3,3 @@ from queue import *
 from graph import *
 from algorithm import *
 from grid import *
-
-
-def astar(number):
-    try:
-        with open("boards/board-"+str(number)+".txt") as f:
-            board = f.read();
-    except FileNotFoundError:
-        print("\n*** BOARD NOT FOUND!***\n")
-        return
-    open_list = []
-    closed_list = []
-    print("board:")
-    print(board)
-
-def main():
-    ans = ""
-    while ans != "q":
-        ans = input("### Choose board!\n(eg. 1-1, 2-3, ..) \n(q => quit) \n-------\n >")
-        if ans == "q":
-            break
-        astar(ans)
-
-main()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from queue import *
 from graph import *
 from algorithm import *
 from grid import *
-
+from parse import *
 
 # data from main article
 DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
@@ -29,4 +29,21 @@ draw_grid(diagram4, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
 
 print("\n\nFrom boards!\n")
 
-board1 = 1
+with open("boards/board-1-1.txt") as f:
+    board = f.read();
+print(board)
+
+print(parse_board(get_board(1-1)))
+
+'''
+
+def main():
+    ans = ""
+    while ans != "q":
+        ans = input("### Choose board!\n(eg. 1-1, 2-3, ..) \n(q => quit) \n-------\n >")
+        if ans == "q":
+            break
+        astar(ans)
+
+main()
+'''
